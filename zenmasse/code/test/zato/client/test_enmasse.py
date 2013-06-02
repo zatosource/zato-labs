85fa048cbb46221c9df10d5bd332de97307efab0
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-06-03 01:01:25 +0200

    Started working on zenmasse tool (will become zato enmasse CLI command)

---
+++ zenmasse/code/test/zato/client/test_enmasse.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2013 Dariusz Suchojad <dsuch at zato.io>
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# stdlib
+from unittest import TestCase
+
+# nose
+from nose.tools import eq_
