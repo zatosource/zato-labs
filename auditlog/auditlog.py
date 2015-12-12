c838f146a8610f35b8e2d255108890964d05067f
Dariusz Suchojad <dsuch-gitlab-com@m.zato.io>
2015-12-12 16:39:45 +0100

    Working on the search command.

--- auditlog/auditlog.py
+++ auditlog/auditlog.py
@@ -15,6 +15,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging, os, sys
 from operator import itemgetter
 
+# Bunch
+from bunch import bunchify
+
 # Click
 import click
 
@@ -29,6 +32,11 @@ from zato.common.util import get_config
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
+class exit_code:
+    invoke_error = 1
+    no_channel_match = 2
+    no_search_match = 3
+
 # ################################################################################################################################
 
 class ZatoClient(AnyServiceInvoker):
@@ -37,13 +45,21 @@ class ZatoClient(AnyServiceInvoker):
         self.cluster_id = None
         self.odb_session = None
 
+    def invoke(self, *args, **kwargs):
+        response = self._invoke(async=False, *args, **kwargs)
+        if response.ok:
+            response.data = bunchify(response.data)
+        return response
+
 # ################################################################################################################################
 
 class AuditLogTool(object):
 
-    def __init__(self, path, prefix):
+    def __init__(self, path, channel_pattern, search_pattern, cid):
         self.path = path
-        self.prefix = prefix if prefix != 'all' else '*'
+        self.channel_pattern = channel_pattern if channel_pattern != 'all' else ''
+        self.search_pattern = search_pattern
+        self.cid = cid
 
     def set_client(self):
 
@@ -63,16 +79,23 @@ class AuditLogTool(object):
         self.client.odb_session = session
 
     def get_items(self):
-        services = self.client.invoke(
+        channels = self.client.invoke(
             'zato.http-soap.get-list', {'cluster_id':self.client.cluster_id, 'connection':'channel', 'transport':'plain_http'})
+        seen_any = False
 
-        for item in sorted(services.data, key=itemgetter('name')):
-            if self.prefix != '*':
-                if item['name'].startswith(self.prefix):
-                    config = self.client.invoke('zato.http-soap.get-audit-config', {'id':item['id']}).data
-                    config['service_id'] = item['id']
-                    config['name'] = item['name']
-                    yield config
+        for item in sorted(channels.data, key=itemgetter('name')):
+            if self.channel_pattern not in item.name:
+                continue
+
+            config = self.client.invoke('zato.http-soap.get-audit-config', {'id':item.id}).data
+            config.service_id = item.id
+            config.name = item.name
+            seen_any = True
+            yield config
+
+        if not seen_any:
+            logging.warn('No channels matching pattern `%s`', self.channel_pattern)
+            sys.exit(exit_code.no_match)
 
     def status(self):
         table = Texttable()
@@ -80,7 +103,7 @@ class AuditLogTool(object):
 
         rows = [['service_name', 'audit_enabled']]
         for item in self.get_items():
-            rows.append((item['name'], item['audit_enabled']))
+            rows.append((item.name, item.audit_enabled))
 
         table.add_rows(rows)
         print(table.draw())
@@ -89,10 +112,11 @@ class AuditLogTool(object):
         verb = 'Enabled' if enable else 'Disabled'
 
         for item in self.get_items():
+            seen_any = True
             response = self.client.invoke('zato.http-soap.set-audit-state', {'id':item['service_id'], 'audit_enabled':enable})
             if not response.ok:
                 logging.error(response.details)
-                sys.exit(1)
+                sys.exit(exit_code.invoke_error)
             logging.info('%s audit log for `%s`' % (verb, item['name']))
 
     def enable(self):
@@ -100,6 +124,15 @@ class AuditLogTool(object):
 
     def disable(self):
         self._set_state(False)
+
+    def search(self):
+        result = []
+        for item in self.get_items():
+            if item.audit_enabled:
+                print(item.name, item.audit_enabled)
+
+        if not result:
+            logging.info('No results')
 
 # ################################################################################################################################
 
@@ -111,20 +144,29 @@ def cli_main():
 
 def get_arg(name):
 
-    @click.command()
-    @click.argument('path', type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True))
-    @click.argument('prefix', default='all')
-    @click.pass_context
-    def _cli_arg(ctx, path, prefix):
-        tool = AuditLogTool(path, prefix)
+    def _cli_arg(ctx, path, channel_pattern, search_pattern=None, cid=None):
+        tool = AuditLogTool(path, channel_pattern, search_pattern, cid)
         tool.set_client()
         getattr(tool, name)()
+
+    _cli_arg = click.pass_context(_cli_arg)
+
+    if name == 'search':
+        _cli_arg = click.argument('search_pattern', required=True)(_cli_arg)
+
+    elif name == 'get':
+        _cli_arg = click.argument('cid', required=True)(_cli_arg)
+
+    _cli_arg = click.argument('channel_pattern', required=True)(_cli_arg)
+    _cli_arg = click.argument('path', type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True))(_cli_arg)
+
+    _cli_arg = click.command()(_cli_arg)
 
     return _cli_arg
 
 # ################################################################################################################################
 
-for name in ('enable', 'disable', 'status'):
+for name in ('enable', 'disable', 'status', 'search'):
     cli_main.add_command(get_arg(name), name)
 
 # ################################################################################################################################
