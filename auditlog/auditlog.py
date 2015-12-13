e40fc0c840c266b4734986cd3420e07445c88667
Dariusz Suchojad <dsuch-gitlab-com@m.zato.io>
2015-12-13 01:51:40 +0100

    Print a texttable on search results.

--- auditlog/auditlog.py
+++ auditlog/auditlog.py
@@ -15,6 +15,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging, os, sys
 from operator import itemgetter
 
+# Arrow
+from arrow import get as arrow_get
+
 # Bunch
 from bunch import bunchify
 
@@ -55,15 +58,28 @@ class ZatoClient(AnyServiceInvoker):
 
 class AuditLogTool(object):
 
-    def __init__(self, path, channel_pattern, search_pattern, cid):
-        self.path = path
-        self.channel_pattern = channel_pattern if channel_pattern != 'all' else ''
-        self.search_pattern = search_pattern
+    def __init__(self, server, channel, query, order_by, batch_size, date_format, cid):
+        self.server = server
+        self.channel = channel if channel != 'all' else ''
+        self.query = query
+        self.order_by = order_by
+        self.batch_size = batch_size
+        self.date_format = date_format
         self.cid = cid
+
+        self.logger = logging.getLogger(__name__)
+
+        self.logger.info('Config server: `%s`', self.server)
+        self.logger.info('Config channel: `%s`', channel)
+        self.logger.info('Config query: `%s`', self.query)
+        self.logger.info('Config order_by: `%s`', self.order_by)
+        self.logger.info('Config batch_size: `%s`', self.batch_size)
+        self.logger.info('Config date_format: `%s`', self.date_format)
+        self.logger.info('Config cid: `%r`', self.cid)
 
     def set_client(self):
 
-        repo_dir = os.path.join(os.path.abspath(os.path.join(self.path)), 'config', 'repo')
+        repo_dir = os.path.join(os.path.abspath(os.path.join(self.server)), 'config', 'repo')
         config = get_config(repo_dir, 'server.conf')
 
         self.client = ZatoClient('http://{}'.format(config.main.gunicorn_bind),
@@ -84,18 +100,19 @@ class AuditLogTool(object):
         seen_any = False
 
         for item in sorted(channels.data, key=itemgetter('name')):
-            if self.channel_pattern not in item.name:
+            if self.channel.lower() not in item.name.lower():
                 continue
 
             config = self.client.invoke('zato.http-soap.get-audit-config', {'id':item.id}).data
             config.service_id = item.id
-            config.name = item.name
+            config.service_name = item.service_name
+            config.channel = item.name
             seen_any = True
             yield config
 
         if not seen_any:
-            logging.warn('No channels matching pattern `%s`', self.channel_pattern)
-            sys.exit(exit_code.no_match)
+            self.logger.warn('No channels matching pattern `%s`', self.channel)
+            sys.exit(exit_code.no_channel_match)
 
     def status(self):
         table = Texttable()
@@ -112,12 +129,11 @@ class AuditLogTool(object):
         verb = 'Enabled' if enable else 'Disabled'
 
         for item in self.get_items():
-            seen_any = True
-            response = self.client.invoke('zato.http-soap.set-audit-state', {'id':item['service_id'], 'audit_enabled':enable})
+            response = self.client.invoke('zato.http-soap.set-audit-state', {'id':item.service_id, 'audit_enabled':enable})
             if not response.ok:
-                logging.error(response.details)
+                self.logger.error(response.details)
                 sys.exit(exit_code.invoke_error)
-            logging.info('%s audit log for `%s`' % (verb, item['name']))
+            self.logger.info('%s audit log for `%s`' % (verb, item.name))
 
     def enable(self):
         self._set_state()
@@ -129,10 +145,43 @@ class AuditLogTool(object):
         result = []
         for item in self.get_items():
             if item.audit_enabled:
-                print(item.name, item.audit_enabled)
+                response = self.client.invoke(
+                    'zato.http-soap.get-audit-item-list',
+                    {'conn_id':item.service_id, 'query':self.query, 'batch_size':self.batch_size})
+                if response.ok:
+
+                    len_data = len(response.data)
+                    noun = 'item' if len_data == 1 else 'items'
+                    self.logger.info('%s %s found in `%s`', len_data, noun, item.service_name)
+
+                    for audit_item in response.data:
+                        audit_item.channel = item.channel
+
+                    result.extend(response.data)
+
+                else:
+                    self.logger.error(response.details)
+                    sys.exit(exit_code.invoke_error)
 
         if not result:
-            logging.info('No results')
+            self.logger.info('No results')
+            sys.exit(exit_code.no_search_match)
+
+        len_result = len(result)
+        noun = 'item' if len_result == 1 else 'items'
+        self.logger.info('%s %s found in total', len_result, noun)
+
+        table = Texttable(150)
+        table.set_cols_dtype(['t', 't', 't', 't', 't', 't'])
+
+        rows = [['#', 'req_time_utc', 'resp_time_utc', 'channel', 'remote_addr', 'cid']]
+        for idx, item in enumerate(sorted(result, key=itemgetter(self.order_by)), 1):
+            req_time_utc = arrow_get(item.req_time_utc).strftime(self.date_format)
+            resp_time_utc = arrow_get(item.resp_time_utc).strftime(self.date_format)
+            rows.append((idx, req_time_utc, resp_time_utc, '\n'.join(item.channel.split(',')), item.remote_addr, item.cid))
+
+        table.add_rows(rows)
+        print(table.draw())
 
 # ################################################################################################################################
 
@@ -144,21 +193,24 @@ def cli_main():
 
 def get_arg(name):
 
-    def _cli_arg(ctx, path, channel_pattern, search_pattern=None, cid=None):
-        tool = AuditLogTool(path, channel_pattern, search_pattern, cid)
+    def _cli_arg(ctx, server, channel, query=None, order_by=None, batch_size=None, date_format=None, cid=None):
+        tool = AuditLogTool(server, channel, query, order_by, batch_size, date_format, cid)
         tool.set_client()
         getattr(tool, name)()
 
     _cli_arg = click.pass_context(_cli_arg)
 
     if name == 'search':
-        _cli_arg = click.argument('search_pattern', required=True)(_cli_arg)
+        _cli_arg = click.option('--date-format', default='%Y-%m-%d %H:%M:%S')(_cli_arg)
+        _cli_arg = click.option('--order-by', default='req_time_utc')(_cli_arg)
+        _cli_arg = click.option('--batch-size', default=100)(_cli_arg)
+        _cli_arg = click.argument('query', default='')(_cli_arg)
 
     elif name == 'get':
         _cli_arg = click.argument('cid', required=True)(_cli_arg)
 
-    _cli_arg = click.argument('channel_pattern', required=True)(_cli_arg)
-    _cli_arg = click.argument('path', type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True))(_cli_arg)
+    _cli_arg = click.argument('channel', required=True)(_cli_arg)
+    _cli_arg = click.argument('server', type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True))(_cli_arg)
 
     _cli_arg = click.command()(_cli_arg)
 
