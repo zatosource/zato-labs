7fa68167fd87ba0071a1c198fea1ef9f4fc692f1
David Wilson <dw@botanicus.net>
2018-01-12 19:28:08 +0530

    GH #815 - ide_deploy - version for pre-3.0 clusters.

---
+++ ide_deploy/code/setup.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2013 Dariusz Suchojad <dsuch at zato.io>
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# flake8: noqa
+
+from setuptools import setup, find_packages
+
+version = '1.0.0'
+
+long_description = description = 'Zato <3.0 service providing the hot-deployment endpoint for IDEs.'
+
+setup(
+      name = 'zato-ide-deploy',
+      version = version,
+
+      author = 'Zato Developers',
+      author_email = 'info@zato.io',
+      url = 'https://zato.io',
+      license = 'GNU Lesser General Public License v3 (LGPLv3)',
+      platforms = 'OS Independent',
+      description = description,
+      long_description = description,
+
+      package_dir = {'':'src'},
+      packages = find_packages('src'),
+      namespace_packages = ['zato'],
+
+      install_requires=[]
+      keywords=('soa eai esb middleware messaging queueing asynchronous integration performance http zeromq framework events agile broker messaging server jms enterprise python middleware clustering amqp nosql websphere mq wmq mqseries ibm amqp zmq'),
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Framework :: Buildout',
+          'Intended Audience :: Customer Service',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Intended Audience :: Healthcare Industry',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Telecommunications Industry',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: C',
+          'Programming Language :: Python :: 2 :: Only',
+          'Programming Language :: Python :: 2.7',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: MacOS :: MacOS X',
+          'Topic :: Communications',
+          'Topic :: Database',
+          'Topic :: Internet',
+          'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+          'Topic :: Internet :: File Transfer Protocol (FTP)',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Software Development :: Object Brokering',
+          ],
+
+      zip_safe = False,
+)
